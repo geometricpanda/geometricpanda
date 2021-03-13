d87f1df1a96694dda9cdf8edd955ff1893ac1fbf
Jim Drury <jim@jimdrury.co.uk>
2021-03-13 12:09:12 +0000

    fix(addon-badges): corrects default badges config to object instead of array

--- libs/storybook-addon-badges/src/lib/manager.tsx
+++ libs/storybook-addon-badges/src/lib/manager.tsx
@@ -10,7 +10,7 @@ export const Manager = () => (
     {({ api, state }: Combo) => {
       const story = api.getData(state.storyId, state.refId);
 
-      const badgesConfig = api.getCurrentParameter<BadgesConfig[]>(PARAM_BADGES_CONFIG_KEY) || [];
+      const badgesConfig = api.getCurrentParameter<BadgesConfig[]>(PARAM_BADGES_CONFIG_KEY) || {};
       const badges = api.getCurrentParameter<string[]>(PARAM_BADGES_KEY) || [];
 
       return story && badges.length
