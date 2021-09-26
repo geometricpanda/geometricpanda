536f02658973f8b3f12c559d55fa000b7e8161dd
Jim Drury <jim@jimdrury.co.uk>
2021-09-26 09:57:55 +0100

    fix(badges): resolves kebab case properties

--- libs/storybook-addon-badges/src/lib/blocks/badge.tsx
+++ libs/storybook-addon-badges/src/lib/blocks/badge.tsx
@@ -6,7 +6,7 @@ import { getBadgeCustomProperty, getTitle } from '../helpers/helpers';
 
 export const BadgeWrapper = styled.span(({ theme }) => ({
   display: 'flex',
-  paddingInline: theme.layoutMargin / 2,
+  paddingInline: theme.layoutMargin / 2
 }));
 
 export const BadgeInner = styled.span<Omit<BadgeProps, 'config'>>(
@@ -16,18 +16,17 @@ export const BadgeInner = styled.span<Omit<BadgeProps, 'config'>>(
     borderStyle: `var(${getBadgeCustomProperty(badge, 'border-style')})`,
     borderWidth: `var(${getBadgeCustomProperty(badge, 'border-width')})`,
     color: `var(${getBadgeCustomProperty(badge, 'color')})`,
-    backgroundColor: `var(${getBadgeCustomProperty(
-      badge,
-      'background-color'
-    )})`,
+    backgroundColor: `var(${getBadgeCustomProperty(badge, 'background-color')})`,
     fontSize: `var(${getBadgeCustomProperty(badge, 'font-size')})`,
     fontFamily: `var(${getBadgeCustomProperty(badge, 'font-family')})`,
-    'font-weight': `var(${getBadgeCustomProperty(badge, 'font-weight')})`,
+    // Typecasting as 'bold' due to custom properties not working with `styled`.
+    fontWeight: `var(${getBadgeCustomProperty(badge, 'font-weight')})` as 'bold',
     lineHeight: `var(${getBadgeCustomProperty(badge, 'line-height')})`,
-    'text-transform': `var(${getBadgeCustomProperty(badge, 'text-transform')})`,
+    // Typecasting as 'uppercase' due to custom properties not working with `styled`.
+    textTransform: `var(${getBadgeCustomProperty(badge, 'text-transform')})` as 'uppercase',
     paddingInline: `var(${getBadgeCustomProperty(badge, 'padding-inline')})`,
     paddingBlock: `var(${getBadgeCustomProperty(badge, 'padding-block')})`,
-    display: 'block',
+    display: 'block'
   })
 );
 
